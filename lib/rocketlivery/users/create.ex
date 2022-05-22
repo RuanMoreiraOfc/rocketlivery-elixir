980a76acab63653ede14d9a03e6da34c4906b8fb
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-22 20:43:25 -0300

    feat: make `FallbackController` more generic

--- lib/rocketlivery/users/create.ex
+++ lib/rocketlivery/users/create.ex
@@ -5,5 +5,12 @@ defmodule Rocketlivery.Users.Create do
     params
     |> User.changeset()
     |> Repo.insert()
+    |> handle_insert_response()
+  end
+
+  defp handle_insert_response({:ok, %User{}} = result), do: result
+
+  defp handle_insert_response({:error, result}) do
+    {:error, %{status: :bad_request, result: result}}
   end
 end
