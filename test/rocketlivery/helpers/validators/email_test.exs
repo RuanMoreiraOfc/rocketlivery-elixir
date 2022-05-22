295cb49990ef4afc6e2747065c77b99e1316c443
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-21 21:59:21 -0300

    test(EmailValidatorHelper): add "`regex/0` succeed when compared against a correct list" case

--- test/rocketlivery/helpers/validators/email_test.exs
+++ test/rocketlivery/helpers/validators/email_test.exs
@@ -12,6 +12,32 @@ defmodule Rocketlivery.Helpers.Validators.EmailTest do
       {:ok, regex: regex, matches: matches, dont_matches: dont_matches}
     end
 
+    test "succeed when compared against a correct list", %{dont_matches: dont_matches} do
+      expected_response = []
+
+      response_list = [
+        "email@email.email",
+        "emai.l@email.email",
+        "ema.i.l@email.email",
+        "emai-l@email.email",
+        "ema-i-l@email.email",
+        "emai--l@email.email",
+        "ema--i--l@email.email",
+        "email@e.mail.email",
+        "email@e.m.ail.email",
+        "email@e-mail.email",
+        "email@e--mail.email",
+        "email@e-m-ail.email",
+        "email@e--m--ail.email",
+        "email@email.e-mail",
+        "email@email.e--mail",
+        "email@email.e-m-ail",
+        "email@email.e--m--ail"
+      ]
+
+      assert Enum.filter(response_list, dont_matches) == expected_response
+    end
+
     test "fail when compared against a failing list", %{matches: matches} do
       expected_response = []
 
