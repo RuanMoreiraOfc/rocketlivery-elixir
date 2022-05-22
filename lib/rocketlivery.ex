7808e7895349a00ddf15949d4b34d827eac69842
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-22 20:56:48 -0300

    feat: delegate `by_id/1` from `UserGet` to main module

--- lib/rocketlivery.ex
+++ lib/rocketlivery.ex
@@ -1,5 +1,7 @@
 defmodule Rocketlivery do
   alias Rocketlivery.Users.Create, as: UserCreate
+  alias Rocketlivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
