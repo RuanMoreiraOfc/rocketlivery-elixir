9d81c90761aadcb2010abafb6ffb911004d5c221
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-21 22:15:01 -0300

    build: add pbkdf2 dependencie

--- config/test.exs
+++ config/test.exs
@@ -28,3 +28,7 @@ config :logger, level: :warn
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# External Libs
+## Pbkdf2
+config :pbkdf2_elixir, :rounds, 1
