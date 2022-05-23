53164b1b70ffc902896a5232a30ea8aeb7e82023
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-22 23:13:27 -0300

    chore: create `ErrorHelper` module

---
+++ lib/rocketlivery/helpers/error.ex
@@ -0,0 +1,41 @@
+defmodule Rocketlivery.Helpers.Error do
+  @keys [:status, :result]
+
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  @doc """
+    Returns a tuple with first value as `:error` and the second value as `reason`
+  """
+  def wrap(reason) do
+    {:error, reason}
+  end
+
+  def build(status, result) do
+    %__MODULE__{
+      status: status,
+      result: result
+    }
+  end
+
+  def build_not_found(result) do
+    :not_found
+    |> build(result)
+  end
+
+  def build_bad_request(result) do
+    :bad_request
+    |> build(result)
+  end
+
+  def build_user_not_found_error do
+    "User not found!"
+    |> build_not_found()
+  end
+
+  def build_id_format_error do
+    "Invalid `id` format!"
+    |> build_bad_request()
+  end
+end
