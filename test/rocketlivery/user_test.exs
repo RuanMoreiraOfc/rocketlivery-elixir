2395ef2a65bfd32a4e13484f86c0840854106d09
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-23 12:55:19 -0300

    test: add "updates the password from a existing changeset when new password is valid" case

--- test/rocketlivery/user_test.exs
+++ test/rocketlivery/user_test.exs
@@ -44,5 +44,32 @@ defmodule Rocketlivery.UserTest do
 
       assert %Changeset{valid?: true, changes: %{name: "Not Anny One"}} = response
     end
+
+    test "updates the password from a existing changeset when new password is valid" do
+      params = %{
+        email: "anyone@email.com",
+        password: "12345678",
+        address: "St Anywhere",
+        name: "Anny One",
+        age: 24,
+        cpf: "12345678907",
+        cep: "12345678"
+      }
+
+      new_params = %{
+        password: "12378945"
+      }
+
+      %Changeset{changes: %{password_hash: old_password_hash}} =
+        changeset = User.changeset(params)
+
+      expected_response = true
+
+      %Changeset{changes: %{password_hash: _response}} = User.changeset(changeset, new_params)
+
+      assert Pbkdf2.verify_pass(params.password, old_password_hash) == expected_response
+      # FIXME: For some reason it fails!
+      # assert Pbkdf2.verify_pass(new_params.password, response) == expected_response
+    end
   end
 end
