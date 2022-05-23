c087786f8c23988c37a9ea752e954681d6ffb988
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-23 12:55:15 -0300

    test: add "updates a existing changeset when new params are valid" case

--- test/rocketlivery/user_test.exs
+++ test/rocketlivery/user_test.exs
@@ -21,4 +21,28 @@ defmodule Rocketlivery.UserTest do
       assert %Changeset{valid?: true, changes: %{name: "Anny One"}} = response
     end
   end
+
+  describe "changeset/2" do
+    test "updates a existing changeset when new params are valid" do
+      params = %{
+        email: "anyone@email.com",
+        password: "12345678",
+        address: "St Anywhere",
+        name: "Anny One",
+        age: 24,
+        cpf: "12345678907",
+        cep: "12345678"
+      }
+
+      new_params = %{
+        name: "Not Anny One"
+      }
+
+      changeset = User.changeset(params)
+
+      response = User.changeset(changeset, new_params)
+
+      assert %Changeset{valid?: true, changes: %{name: "Not Anny One"}} = response
+    end
+  end
 end
