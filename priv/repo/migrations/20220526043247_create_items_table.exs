04c2b39e91f8438dec937aad46d168222ff5f8d4
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-26 01:40:17 -0300

    chore: add `CreateItemsTable` migration

---
+++ priv/repo/migrations/20220526043247_create_items_table.exs
@@ -0,0 +1,15 @@
+defmodule Rocketlivery.Repo.Migrations.CreateItemsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:items) do
+      add :category, :item_category
+      add :name, :string
+      add :description, :string
+      add :price, :decimal
+      add :photo, :string
+
+      timestamps()
+    end
+  end
+end
