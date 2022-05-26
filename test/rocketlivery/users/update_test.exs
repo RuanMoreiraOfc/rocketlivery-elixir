c3452c2701254b4d37a77391cd5038a810a3009c
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-26 08:39:36 -0300

    test: add "fails to update an user in database when any param is invalid" case

--- test/rocketlivery/users/update_test.exs
+++ test/rocketlivery/users/update_test.exs
@@ -3,6 +3,7 @@ defmodule Rocketlivery.Users.UpdateTest do
 
   import Rocketlivery.Factory
 
+  alias Ecto.Changeset
   alias Rocketlivery.User
   alias Rocketlivery.Users.Update
 
@@ -14,6 +15,15 @@ defmodule Rocketlivery.Users.UpdateTest do
       response = Update.call(new_params)
 
       assert {:ok, %User{id: ^id, age: 200}} = response
+    end
+
+    test "fails to update an user in database when any param is invalid" do
+      %{id: id} = insert(:user)
+      new_params = %{"id" => id, "age" => 17}
+
+      response = Update.call(new_params)
+
+      assert {:error, %Changeset{errors: _errors}} = response
     end
   end
 end
