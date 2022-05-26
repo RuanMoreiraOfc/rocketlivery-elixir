c9c762c71bbba4d15cc7417726e89504d54430c2
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-26 09:30:29 -0300

    chore(Factory): add `item_factory`

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Rocketlivery.Factory do
   use ExMachina.Ecto, repo: Rocketlivery.Repo
 
-  alias Rocketlivery.User
+  alias Rocketlivery.{Item, User}
 
   def user_params_factory do
     %{
@@ -30,6 +30,17 @@ defmodule Rocketlivery.Factory do
 
   def item_params_factory do
     %{
+      category: :drink,
+      name: "Apple Juice",
+      description: "Juice made of apples",
+      price: Decimal.new("2.5") |> Decimal.to_float(),
+      photo: "/priv/static/assets/image.jpg"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      id: "7c6576f9-9161-4bba-8b94-d997c6d0f4a1",
       category: :drink,
       name: "Apple Juice",
       description: "Juice made of apples",
