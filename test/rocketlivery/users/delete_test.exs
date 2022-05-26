c206515a5218b3545f1e55fe89d466382438b108
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-26 08:18:47 -0300

    test: add "fails to delete an user in database when id is invalid" case

--- test/rocketlivery/users/delete_test.exs
+++ test/rocketlivery/users/delete_test.exs
@@ -3,6 +3,7 @@ defmodule Rocketlivery.Users.DeleteTest do
 
   import Rocketlivery.Factory
 
+  alias Rocketlivery.Helpers.Error
   alias Rocketlivery.User
   alias Rocketlivery.Users.Delete
 
@@ -14,5 +15,14 @@ defmodule Rocketlivery.Users.DeleteTest do
 
       assert {:ok, %User{id: ^id}} = response
     end
+  end
+
+  test "fails to delete an user in database when id is invalid" do
+    %{id: id_from_database} = insert(:user)
+    id = String.replace(id_from_database, ~r/.$/, &if(&1 == "0", do: "1", else: "0"))
+
+    response = Delete.call(id)
+
+    assert {:error, %Error{status: :not_found, result: _result}} = response
   end
 end
