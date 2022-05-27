9e793f30a2bbca5b34748d38765b1140f9b5c004
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-27 00:09:33 -0300

    test: add "fails to show the item via `conn` when id has not been found" case

--- test/rocketlivery_web/controllers/items_controller_test.exs
+++ test/rocketlivery_web/controllers/items_controller_test.exs
@@ -70,5 +70,16 @@ defmodule RocketliveryWeb.ItemsControllerTest do
 
       assert %{"item" => %{"id" => ^id}} = response
     end
+
+    test "fails to show the item via `conn` when id has not been found", %{conn: conn} do
+      id = "7c6576f9-9161-4bba-8b94-d997c6d0f4a1"
+
+      response =
+        conn
+        |> get(Routes.items_path(conn, :show, id))
+        |> json_response(:not_found)
+
+      assert %{"message" => "Item not found!"} = response
+    end
   end
 end
