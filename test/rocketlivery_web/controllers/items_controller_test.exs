78e020b241221e211ad8b00ab94a3299c974b8c8
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-27 00:07:22 -0300

    test: add "fails to create the item via `conn` when any param is invalid" case

--- test/rocketlivery_web/controllers/items_controller_test.exs
+++ test/rocketlivery_web/controllers/items_controller_test.exs
@@ -14,5 +14,20 @@ defmodule RocketliveryWeb.ItemsControllerTest do
 
       assert %{"message" => "Item created!", "item" => %{}} = response
     end
+
+    test "fails to create the item via `conn` when any param is invalid", %{conn: conn} do
+      params = string_params_for(:item_params, category: "any")
+
+      response =
+        conn
+        |> post(Routes.items_path(conn, :create, params))
+        |> json_response(:bad_request)
+
+      assert %{
+               "message" => %{
+                 "category" => ["is invalid, valid options are: ['food', 'drink', 'desert']"]
+               }
+             } = response
+    end
   end
 end
