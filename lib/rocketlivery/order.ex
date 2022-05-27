2f2de1a32ce5cd2ce96367709dcca7585ecf434f
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-27 00:41:16 -0300

    feat: create `Order` schema

---
+++ lib/rocketlivery/order.ex
@@ -0,0 +1,40 @@
+defmodule Rocketlivery.Order do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Ecto.Enum
+  alias Rocketlivery.{Item, User}
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  @required_params [
+    :user_id,
+    :address,
+    :payment_method,
+    :notes
+  ]
+  @derive {Jason.Encoder, only: [:id, :items] ++ @required_params}
+
+  @payment_method_enum [:money, :credit_card, :debit_card]
+
+  schema "orders" do
+    many_to_many :items, Item, join_through: "orders_items"
+
+    belongs_to :user, User
+    field :address, :string
+    field :payment_method, Enum, values: @payment_method_enum
+    field :notes, :string
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params, items) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> put_assoc(:items, items)
+
+    # end_validation_pipe_line
+  end
+end
