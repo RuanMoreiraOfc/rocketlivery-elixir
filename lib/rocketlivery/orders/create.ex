fbab521c6a25dec213377a619d4bfb0e37a59464
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-30 02:26:48 -0300

    refactor(OrderCreate): use `ParseItemsParamsHelper`

--- lib/rocketlivery/orders/create.ex
+++ lib/rocketlivery/orders/create.ex
@@ -1,59 +1,13 @@
 defmodule Rocketlivery.Orders.Create do
-  import Ecto.Query
-
   alias Rocketlivery.Helpers.Error, as: ErrorHelper
-  alias Rocketlivery.{Item, Order, Repo}
+  alias Rocketlivery.Helpers.ParseItemsParams, as: ParseItemsParamsHelper
+  alias Rocketlivery.{Order, Repo}
 
   def call(params) do
     params
-    |> fetch_items()
-    |> validate_items()
-    |> transform_items()
+    |> ParseItemsParamsHelper.call()
     |> handle_insert(params)
     |> handle_insert_response()
-  end
-
-  defp fetch_items(%{"items" => itemsFromParams}) do
-    items_ids = Enum.map(itemsFromParams, & &1["id"])
-
-    query = from item in Item, where: item.id in ^items_ids
-
-    items_from_db = Repo.all(query)
-
-    {itemsFromParams, items_from_db}
-  end
-
-  defp validate_items({itemsFromParams, itemsFromDB} = itemsTuple) do
-    ids_count =
-      itemsFromParams
-      |> Enum.uniq_by(& &1["id"])
-      |> Enum.count()
-
-    ids_from_db_count =
-      itemsFromDB
-      |> Enum.uniq_by(& &1.id)
-      |> Enum.count()
-
-    is_valid = ids_count == ids_from_db_count
-
-    {is_valid, itemsTuple}
-  end
-
-  defp transform_items({false, _itemsTuple}) do
-    {:error, "Invalid ID's!"}
-  end
-
-  defp transform_items({true, {itemsFromParams, itemsFromDB}}) do
-    result =
-      itemsFromParams
-      |> Enum.map(fn item ->
-        item_from_db = Enum.find(itemsFromDB, &(&1.id == item["id"]))
-
-        List.duplicate([item_from_db], item["quantity"])
-      end)
-      |> List.flatten()
-
-    {:ok, result}
   end
 
   defp handle_insert({:error, _reason} = result, _params), do: result
