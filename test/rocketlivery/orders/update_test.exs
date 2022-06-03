7818e2a2209881857aa7765987c45f2196c38383
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-03 11:23:44 -0300

    test: add "fails to update an order in database when any param is invalid" case

--- test/rocketlivery/orders/update_test.exs
+++ test/rocketlivery/orders/update_test.exs
@@ -3,6 +3,8 @@ defmodule Rocketlivery.Orders.UpdateTest do
 
   import Rocketlivery.Factory
 
+  alias Ecto.Changeset
+  alias Rocketlivery.Helpers.Error
   alias Rocketlivery.Order
   alias Rocketlivery.Orders.Update
 
@@ -15,6 +17,19 @@ defmodule Rocketlivery.Orders.UpdateTest do
       response = Update.call(new_params)
 
       assert {:ok, %Order{id: ^id, notes: ^notes}} = response
+    end
+
+    test "fails to update an order in database when any param is invalid" do
+      %{id: id} = insert(:order)
+      new_params = %{"id" => id, "notes" => nil}
+
+      response = Update.call(new_params)
+
+      assert {:error,
+              %Error{
+                status: :bad_request,
+                result: %Changeset{errors: _errors}
+              }} = response
     end
   end
 end
