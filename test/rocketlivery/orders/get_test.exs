eaf1d270898a4ccbf6983835ae316e4cf1b42ccf
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-03 11:22:49 -0300

    test: add "fails to get an order by id from database when id is invalid" case

--- test/rocketlivery/orders/get_test.exs
+++ test/rocketlivery/orders/get_test.exs
@@ -3,6 +3,7 @@ defmodule Rocketlivery.Orders.GetTest do
 
   import Rocketlivery.Factory
 
+  alias Rocketlivery.Helpers.Error
   alias Rocketlivery.Order
   alias Rocketlivery.Orders.Get
 
@@ -14,5 +15,14 @@ defmodule Rocketlivery.Orders.GetTest do
 
       assert {:ok, %Order{id: ^id}} = response
     end
+  end
+
+  test "fails to get an order by id from database when id is invalid" do
+    %{id: id_from_database} = insert(:order)
+    id = String.replace(id_from_database, ~r/.$/, &if(&1 == "0", do: "1", else: "0"))
+
+    response = Get.by_id(id)
+
+    assert {:error, %Error{status: :not_found, result: _result}} = response
   end
 end
