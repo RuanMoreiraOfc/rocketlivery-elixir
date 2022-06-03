f8d160f92ab9122abf7acb2add949c0c861ee099
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-03 11:23:56 -0300

    test: add "fails to update an order in database when id is invalid" case

--- test/rocketlivery/orders/update_test.exs
+++ test/rocketlivery/orders/update_test.exs
@@ -31,5 +31,15 @@ defmodule Rocketlivery.Orders.UpdateTest do
                 result: %Changeset{errors: _errors}
               }} = response
     end
+
+    test "fails to update an order in database when id is invalid" do
+      %{id: id_from_database} = insert(:order)
+      id = String.replace(id_from_database, ~r/.$/, &if(&1 == "0", do: "1", else: "0"))
+      new_params = %{"id" => id}
+
+      response = Update.call(new_params)
+
+      assert {:error, %Error{status: :not_found, result: _result}} = response
+    end
   end
 end
