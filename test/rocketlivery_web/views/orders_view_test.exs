36be14e3a66424c642e934d6e0744e681778f47f
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-03 11:25:40 -0300

    test(OrderView): add "renders order.json" case

--- test/rocketlivery_web/views/orders_view_test.exs
+++ test/rocketlivery_web/views/orders_view_test.exs
@@ -34,4 +34,31 @@ defmodule RocketliveryWeb.OrdersViewTest do
              }
            } = response
   end
+
+  test "renders order.json" do
+    price = Decimal.new("2.5") |> Decimal.to_float()
+    order = build(:order, notes: "more sugar")
+
+    response = render(OrdersView, "order.json", order: order)
+
+    assert %{
+             order: %Order{
+               id: "4b9eb78b-a91f-4e6c-bda4-01b462f946ff",
+               user_id: "b815baa2-f7a4-4eeb-ad2b-2790d48cbbf3",
+               items: [
+                 %Item{
+                   id: "7c6576f9-9161-4bba-8b94-d997c6d0f4a1",
+                   category: :drink,
+                   name: "Apple Juice",
+                   description: "Juice made of apples",
+                   price: ^price,
+                   photo: "/priv/static/assets/image.jpg"
+                 }
+               ],
+               address: "St anywhere",
+               payment_method: :money,
+               notes: "more sugar"
+             }
+           } = response
+  end
 end
