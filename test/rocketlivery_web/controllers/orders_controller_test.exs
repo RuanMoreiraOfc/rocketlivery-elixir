40bed0abb22abc4d21fdbd74ad71ac4a61ce1299
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-03 11:27:46 -0300

    test: add "fails to create the order via `conn` when any param is invalid" todo

--- test/rocketlivery_web/controllers/orders_controller_test.exs
+++ test/rocketlivery_web/controllers/orders_controller_test.exs
@@ -14,5 +14,16 @@ defmodule RocketliveryWeb.OrdersControllerTest do
 
   #     assert %{"message" => "Order created!", "order" => %{}} = response
   #   end
+
+  #   test "fails to create the order via `conn` when any param is invalid", %{conn: conn} do
+  #     params = string_params_for(:order_params, user_id: nil)
+
+  #     response =
+  #       conn
+  #       |> post(Routes.orders_path(conn, :create, params))
+  #       |> json_response(:bad_request)
+
+  #     assert %{"message" => %{"user_id" => ["can't be blank"]}} = response
+  #   end
   # end
 end
