bfc84d95f16a05b51324020412c70ec07c2557fc
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-03 11:28:17 -0300

    test: add "deletes the order via `conn` when id has been found" case

--- test/rocketlivery_web/controllers/orders_controller_test.exs
+++ test/rocketlivery_web/controllers/orders_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule RocketliveryWeb.OrdersControllerTest do
   use RocketliveryWeb.ConnCase, async: true
 
+  import Rocketlivery.Factory
+
   # This test always gonna fail because Plug.Query.encode_pair can't parse a nested maps param
   # TODO
   # describe "create/2" do
@@ -26,4 +28,19 @@ defmodule RocketliveryWeb.OrdersControllerTest do
   #     assert %{"message" => %{"user_id" => ["can't be blank"]}} = response
   #   end
   # end
+
+  describe "delete/2" do
+    test "deletes the order via `conn` when id has been found", %{conn: conn} do
+      id = "4b9eb78b-a91f-4e6c-bda4-01b462f946ff"
+
+      insert(:order, id: id)
+
+      response =
+        conn
+        |> delete(Routes.orders_path(conn, :delete, id))
+        |> json_response(:ok)
+
+      assert %{"order" => %{"id" => ^id}} = response
+    end
+  end
 end
