c91e7a3bf28f3ea676ecdd674921f9ad1aa51cc4
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-06 17:13:14 -0300

    fix(OrdersControllerTest): fix `create/2` cases

--- test/rocketlivery_web/controllers/orders_controller_test.exs
+++ test/rocketlivery_web/controllers/orders_controller_test.exs
@@ -3,31 +3,29 @@ defmodule RocketliveryWeb.OrdersControllerTest do
 
   import Rocketlivery.Factory
 
-  # This test always gonna fail because Plug.Query.encode_pair can't parse a nested maps param
-  # TODO
-  # describe "create/2" do
-  #   test "creates the order via `conn` when all params are valid", %{conn: conn} do
-  #     params = string_params_for(:order_params)
+  describe "create/2" do
+    test "creates the order via `conn` when all params are valid", %{conn: conn} do
+      params = string_params_for(:order_params)
 
-  #     response =
-  #       conn
-  #       |> post(Routes.orders_path(conn, :create, params))
-  #       |> json_response(:created)
+      response =
+        conn
+        |> post(Routes.orders_path(conn, :create), params)
+        |> json_response(:created)
 
-  #     assert %{"message" => "Order created!", "order" => %{}} = response
-  #   end
+      assert %{"message" => "Order created!", "order" => %{}} = response
+    end
 
-  #   test "fails to create the order via `conn` when any param is invalid", %{conn: conn} do
-  #     params = string_params_for(:order_params, user_id: nil)
+    test "fails to create the order via `conn` when any param is invalid", %{conn: conn} do
+      params = string_params_for(:order_params, user_id: nil)
 
-  #     response =
-  #       conn
-  #       |> post(Routes.orders_path(conn, :create, params))
-  #       |> json_response(:bad_request)
+      response =
+        conn
+        |> post(Routes.orders_path(conn, :create), params)
+        |> json_response(:bad_request)
 
-  #     assert %{"message" => %{"user_id" => ["can't be blank"]}} = response
-  #   end
-  # end
+      assert %{"message" => %{"user_id" => ["can't be blank"]}} = response
+    end
+  end
 
   describe "delete/2" do
     test "deletes the order via `conn` when id has been found", %{conn: conn} do
