f330cfcd6bff5da74e010a0f845efa047a9762e9
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-06 22:07:11 -0300

    feat(AuthGuardian): create `authenticate` function

--- lib/rocketlivery_web/auth/guardian.ex
+++ lib/rocketlivery_web/auth/guardian.ex
@@ -1,6 +1,7 @@
 defmodule RocketliveryWeb.Auth.Guardian do
   use Guardian, otp_app: :rocketlivery
 
+  alias Rocketlivery.Helpers.Error, as: ErrorHelper
   alias Rocketlivery.User
   alias Rocketlivery.Users.Get, as: UserGet
 
@@ -10,5 +11,27 @@ defmodule RocketliveryWeb.Auth.Guardian do
     claims
     |> Map.get("sub")
     |> UserGet.by_id()
+  end
+
+  def authenticate(%{"id" => id, "password" => password}) do
+    with {:ok, %User{password_hash: hash} = user} <- UserGet.by_id(id),
+         true <- Pbkdf2.verify_pass(password, hash),
+         {:ok, token, _claims} <- encode_and_sign(user) do
+      {:ok, token}
+    else
+      false ->
+        "Please verify your credentials!"
+        |> ErrorHelper.build_unauthorized()
+        |> ErrorHelper.wrap()
+
+      error ->
+        error
+    end
+  end
+
+  def authenticate(_) do
+    "Invalid or missing params"
+    |> ErrorHelper.build_bad_request()
+    |> ErrorHelper.wrap()
   end
 end
