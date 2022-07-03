ccf48df862795584f83f8b52531ce71bc8de21fd
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-07-03 00:07:09 -0300

    chore: ignore not owner code from tests

--- lib/rocketlivery_web/router.ex
+++ lib/rocketlivery_web/router.ex
@@ -35,6 +35,7 @@ defmodule RocketliveryWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
+  # coveralls-ignore-start
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
@@ -56,4 +57,6 @@ defmodule RocketliveryWeb.Router do
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
+
+  # coveralls-ignore-stop
 end
