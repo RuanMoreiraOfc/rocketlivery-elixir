dfe0fd5a5df1eb42c47683917f3794cff384a400
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-07-04 13:19:26 -0300

    chore: setup `runtime` config for releases

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,20 +48,33 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  app_name = System.get_env("APP_NAME")
 
-  config :rocketlivery, RocketliveryWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
-    secret_key_base: secret_key_base
+  if app_name do
+    host = "#{app_name}.gigalixirapp.com"
+    port = {:system, "PORT"}
+
+    config :rocketlivery, RocketliveryWeb.Endpoint,
+      server: true,
+      http: [port: port],
+      url: [host: host, port: 443],
+      secret_key_base: secret_key_base
+  else
+    host = System.get_env("PHX_HOST") || "example.com"
+    port = String.to_integer(System.get_env("PORT") || "4000")
+
+    config :rocketlivery, RocketliveryWeb.Endpoint,
+      url: [host: host, port: 443, scheme: "https"],
+      http: [
+        # Enable IPv6 and bind on all interfaces.
+        # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+        # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+        # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+        ip: {0, 0, 0, 0, 0, 0, 0, 0},
+        port: port
+      ],
+      secret_key_base: secret_key_base
+  end
 
   # ## Configuring the mailer
   #
